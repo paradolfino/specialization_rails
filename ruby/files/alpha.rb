531deddf611aae90bd81ffca2434ab994038d978
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-06 20:23:52 -0600

    commit to pool[n5bd7r] at 20:23 - 06/01/2018

---
+++ ruby/files/alpha.rb
@@ -0,0 +1,17 @@
+# File.foreach( 'alpha.rb' ) do |line|
+#     puts line
+#     p line
+#     p line.chomp
+#     p line.split
+# end
+
+puts "Give filename:"
+input = gets.chomp
+
+begin
+    File.foreach( input ) do |l|
+        puts l
+    end
+rescue => e
+    p e
+end
