da300fe3bfc279a1bc81df98e1f6f3e8b357423c
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-06 20:32:48 -0600

    commit to pool[n5bd7r] at 20:32 - 06/01/2018

--- ruby/files/beta.rb
+++ ruby/files/beta.rb
@@ -1,14 +1,14 @@
 ##WRITING & READING
 fname = "beta.rb"
-File.open("test.rb", "w") do |f|
+File.open(fname, "w") do |f|
     f.puts "Test"
 end
 
 test_arr = []
 
-File.foreach("test.rb") do |l|
+File.foreach(fname) do |l|
     test_arr.push(l.chomp)
 end
 
-File.open("test.rb", "w") {|f| f.puts test_arr.join }
+File.open(fname, "w") {|f| f.puts test_arr.join }
 
