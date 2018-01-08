6114e1bfe48aecbbff7cc24d0bbc17b7a5039e85
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-08 14:18:36 -0600

    commit to pool[7q065l] at 14:18 - 08/01/2018

--- ruby/module_2/assignment_1/module2_lesson1_formative.rb
+++ ruby/module_2/assignment_1/module2_lesson1_formative.rb
@@ -20,7 +20,11 @@ another_var = "nil"
 case
 when some_var == "pink elephant"
   puts "Don't think about the pink elephant!"
+when another_var.nil?
+  puts "Question mark in the method name?"
+when some_var == false
+  puts "Looks like this one should execute"
 else
-  puts "Don't think about the pink elephansssst!"
+  puts "I guess nothing matched... But why?"
 end
 
