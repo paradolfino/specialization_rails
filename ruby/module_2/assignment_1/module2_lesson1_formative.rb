55f6e805545ded0111f33138a8e29b0f72034379
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-08 14:17:14 -0600

    commit to pool[7q065l] at 14:17 - 08/01/2018

--- ruby/module_2/assignment_1/module2_lesson1_formative.rb
+++ ruby/module_2/assignment_1/module2_lesson1_formative.rb
@@ -24,15 +24,3 @@ else
   puts "Don't think about the pink elephansssst!"
 end
 
-# if some_var == "pink elephant"
-#   puts "Don't think about the pink elephant!"
-
-# elsif another_var == "nil"
-#   puts "Question mark in the method name?"
-
-# elsif some_var == false
-#   puts "Looks like this one should execute"
-
-# else
-#   puts "I guess nothing matched... But why?"
-# end
