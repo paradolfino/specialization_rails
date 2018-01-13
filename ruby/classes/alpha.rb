08d39ad561b43eebe3cb10570c1f130a63e4da04
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-12 20:10:30 -0600

    commit to pool[526az3] at 20:10 - 12/01/2018

--- ruby/classes/alpha.rb
+++ ruby/classes/alpha.rb
@@ -1,23 +1,23 @@
-class Person
-    #attr_accessor :name, :age
-    #attr_writer
-    #attr_reader
-    def initialize (name: "default", age: "def age")
-        @name = name
-        @age = age
-    end
-    def get_info
-        @additional_info = "Interesting"
-        "Name: #{@name}, age: #{@age}"
-    end
-    def name #getter
-        @name
-    end
-    def name= (new_name)
-        @name = new_name #setter
-    end
-end
+# class Person
+#     #attr_accessor :name, :age
+#     #attr_writer
+#     #attr_reader
+#     def initialize (name: "default", age: "def age")
+#         @name = name
+#         @age = age
+#     end
+#     def get_info
+#         @additional_info = "Interesting"
+#         "Name: #{@name}, age: #{@age}"
+#     end
+#     def name #getter
+#         @name
+#     end
+#     def name= (new_name)
+#         @name = new_name #setter
+#     end
+# end
 
-per = Person.new
+# per = Person.new
 
-p per
+# p per
