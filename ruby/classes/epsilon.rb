afdbc5442f4dcb6830c9cbef3103c9f4de3bb6d6
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-12 20:28:58 -0600

    commit to pool[526az3] at 20:28 - 12/01/2018

--- ruby/classes/epsilon.rb
+++ ruby/classes/epsilon.rb
@@ -1,1 +1,15 @@
 #inheritance
+
+class Dog
+    def to_s
+        "Dog"
+    end
+    def bark
+        "barks loudly"
+    end
+end
+class SmallDog < Dog
+    def bark #override
+        "barks quietly"
+    end
+end
