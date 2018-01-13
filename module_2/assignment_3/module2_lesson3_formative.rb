b80d8ab06b75893bf6dfedcfefa65561f72686e6
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-13 17:51:29 -0600

    commit to pool[o032h4] at 17:51 - 13/01/2018

--- module_2/assignment_3/module2_lesson3_formative.rb
+++ module_2/assignment_3/module2_lesson3_formative.rb
@@ -11,7 +11,7 @@ class Person
     #add the created instance (self) to people class variable
     self.first_name = first_name
     self.last_name = last_name
-    @@people.push(self)
+    @@people << self
   end
 
   #have a `search` method to locate all people with a matching `last_name`
