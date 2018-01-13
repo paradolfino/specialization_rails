b858c37ec085cd5c1fa23bde6360efaa04fd9422
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-13 17:53:22 -0600

    commit to pool[o032h4] at 17:53 - 13/01/2018

--- module_2/assignment_3/module2_lesson3_formative.rb
+++ module_2/assignment_3/module2_lesson3_formative.rb
@@ -1,30 +1,20 @@
 class Person
-  #have a first_name and last_name attribute with public accessors
-  #attr_accessor 
   attr_accessor :first_name, :last_name
-  #have a class attribute called `people` that holds an array of objects
+
   @@people = []
 
-  #have an `initialize` method to initialize each instance
-  def initialize(first_name,last_name)#should take 2 parameters for first_name and last_name
-    #assign those parameters to instance variables
-    #add the created instance (self) to people class variable
-    self.first_name = first_name
-    self.last_name = last_name
-    @@people << self
+  def initialize(first_name, last_name)
+  	self.first_name = first_name
+  	self.last_name = last_name 
+  	@@people << self
   end
 
-  #have a `search` method to locate all people with a matching `last_name`
   def self.search(last_name)
-    #accept a `last_name` parameter
-    #search the `people` class attribute for instances with the same `last_name`
-    #return a collection of matching instances
-    @@people.select {|person| person.last_name == last_name }
+    @@people.select {|person| person.last_name == last_name}
   end
 
-  #have a `to_s` method to return a formatted string of the person's name
+  # String representation of the class used by puts
   def to_s
-    #return a formatted string as `first_name(space)last_name`
     "#{first_name} #{last_name}"
   end
 end
