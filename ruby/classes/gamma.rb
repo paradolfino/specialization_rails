afb0c9fe3ae8f39343f9c7e824805bee26cc48ad
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-12 20:19:42 -0600

    commit to pool[526az3] at 20:19 - 12/01/2018

--- ruby/classes/gamma.rb
+++ ruby/classes/gamma.rb
@@ -6,3 +6,17 @@
 # @x = @x || 5
 
 # Short Form @ ||= 5
+
+class Person
+    attr_reader :age
+    attr_accessor :name
+
+    def initialize (name, age)
+        @name = name
+        self.age = age
+    end
+    def age= (new_age)
+        @age ||=5
+        @age = new_age unless new_age > 120
+    end
+end
