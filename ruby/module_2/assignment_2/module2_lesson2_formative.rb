0310ab5a3b8a9fa70d7e86518cb8263d17d770ff
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-12 19:52:37 -0600

    commit to pool[15u8wp] at 19:52 - 12/01/2018

--- ruby/module_2/assignment_2/module2_lesson2_formative.rb
+++ ruby/module_2/assignment_2/module2_lesson2_formative.rb
@@ -1,10 +1,10 @@
 # Grab 23 random elements between 0 and 10000
 arr = (1..10000).to_a.sample(23)
-p arr
+# p arr
 
 # This selects only elements that when divided by 3 have a remainder of 0 
 # using the % (modulus) operator
-p arr.select { |element| element % 3 == 0 }
+p arr.select{ |element| element % 3 == 0 }.reject{ |x| x < 5000 }.reverse
 
 # Using `reject` method filter out anything less than 5000
 # and use `sort` and `reverse` methods to sort in descending order
