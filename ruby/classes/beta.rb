62893c3e0025f1721dae4de1c5b47332559091d2
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-12 20:11:43 -0600

    commit to pool[526az3] at 20:11 - 12/01/2018

---
+++ ruby/classes/beta.rb
@@ -0,0 +1,12 @@
+class Person
+    attr_reader :age
+    attr_accessor :name
+
+    def initialize(name, ageVar)
+        @name = name
+        self.age = ageVar
+        puts age
+    end
+    def age= (new_age)
+        @age = new_age unless new)age > 120
+    end
