53661c567cd89ec10f22d015cf74167a399609b1
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-12 20:25:47 -0600

    commit to pool[526az3] at 20:25 - 12/01/2018

---
+++ ruby/classes/delta.rb
@@ -0,0 +1,15 @@
+# multiple ways to define class methods
+
+class MatFun
+    def self.dble(var) #self
+        times_called; var * 2;
+    end
+    class << self # << self
+        def times_called
+            @@times_called ||=0; @times_called += 1
+        end
+    end
+end
+def MatFun.tple(var) #outside of class
+    times_called; var * 3
+end
