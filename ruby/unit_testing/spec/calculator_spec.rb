dab72d9fdd2af81d0ffc9fc49982c86b0c2f61a7
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-14 17:29:56 -0600

    commit to pool[vy43r1] at 17:29 - 14/01/2018

--- ruby/unit_testing/spec/calculator_spec.rb
+++ ruby/unit_testing/spec/calculator_spec.rb
@@ -7,3 +7,8 @@ describe Calculator do
     it "should add 2 numbers correctly" do
         expect(@calculator.add(2,2)).to eq 4
     end
+
+    it "should subtract 2 numbers correctly" do
+        expect(@calculator.subtract(5,5)).to eq 0
+    end
+end
