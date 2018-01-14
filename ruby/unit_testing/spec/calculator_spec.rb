f1e33420d36b00d12e629ce009c63510ad39e805
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-14 17:29:00 -0600

    commit to pool[vy43r1] at 17:29 - 14/01/2018

--- ruby/unit_testing/spec/calculator_spec.rb
+++ ruby/unit_testing/spec/calculator_spec.rb
@@ -3,3 +3,7 @@ require_relative '../calculator'
 
 describe Calculator do
     before { @calculator = Calculator.new('RSpec calculator')}
+
+    it "should add 2 numbers correctly" do
+        expect(@calculator.add(2,2)).to eq 4
+    end
