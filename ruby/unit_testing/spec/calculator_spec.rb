b3659b299bc250b8ee24e1d684a14c81ea03bfae
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-14 17:35:25 -0600

    commit to pool[vy43r1] at 17:35 - 14/01/2018

--- ruby/unit_testing/spec/calculator_spec.rb
+++ ruby/unit_testing/spec/calculator_spec.rb
@@ -13,6 +13,10 @@ describe Calculator do
     it "should subtract 2 numbers correctly" do
         expect(@calculator.subtract(5,5)).not_to eq 10
     end
+
+    it "should sum 2 odd numbers and return an even result"
+        expect(@calculator.sum_even(5,5)).to be_even
+    end
 end
 
 #be_ predicate for booleans such as be_even, be_odd, be_nil
